056cb8168de1940f073860130decb99cc80afd63
ptkmc <23647036+ptkmc@users.noreply.github.com>
2019-05-21 16:16:17 -0500

    Add quick start example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
+const gradient = { background: 'linear-gradient(180deg,#7c69f4,#5546f2)' };
+
+const Home = () => (
+  <div style={gradient}>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Topic = ({ match }) => {
+  return <h3>{match.params.topicId}</h3>;
+};
+
+const Topics = ({ match }) => {
+  console.log(match);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Topics</h2>
+      <Route
+        exact
+        path={match.url}
+        render={() => <h3>Please select a topic</h3>}
+      />
+      <ul>
+        <li>
+          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to="/topics/components">Components</Link>
+        </li>
+        <li>
+          <Link to="/topics/props-v-state">Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path="/topics/:topicId" component={Topic} />
     </div>
   );
+};
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+        </div>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </Router>
+    );
+  }
 }
 
 export default App;
